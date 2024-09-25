3cd1790994b1cf2de2ef00f7c4c83bdc8af2284a
komandu5 <komandu5@msu.edu>
2024-09-25 13:13:58 -0400

    started print functionality- 4 tests pass

--- Project2.cpp
+++ Project2.cpp
@@ -26,6 +26,52 @@ int main(int argc, char * argv[])
     std::cout << "ERROR: Unable to open file '" << filename << "'." << std::endl;
     exit(1);
   }
+  std::ifstream input(filename);
+  emplex::Lexer lexer;
+  std::vector<emplex::Token> tokens = lexer.Tokenize(input);
+  bool print_active = false;
+  bool condition_active = false;
+  std::string output_string;
+  for (emplex::Token token: tokens)
+  {
+    if(token.id != -1){
+      if(token.id == 252)
+      {
+        print_active = true;
+      }
+      else if(print_active and token.id == 245)
+      {
+        condition_active = true;
+      }
+      else if(token.id == 243)
+      {
+        condition_active = false;
+      }
+      else if(condition_active and token.id == 244)
+      {
+        for(auto ch : token.lexeme)
+        {
+          if(ch != '"'){
+            output_string += ch;
+          }
+        }
+      }
+      else if(condition_active and token.id == 247)
+      {
+        output_string += token.lexeme;
+      }
+      else if(token.id == 246)
+      {
+        if(print_active)
+        {
+          print_active = false;
+          std::cout << output_string << std::endl;
+        }
+      }
+      
+    }
+
+  }
 
   // TO DO:  
   // PARSE input file to create Abstract Syntax Tree (AST).
