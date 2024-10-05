8d691b8568f662f1eed1598040bbc114f3970712
AlexBanker <143361539+AlexBanker@users.noreply.github.com>
2024-10-05 12:28:37 -0400

    Test
    This reverts commit 2cd4707ffa932c02191486020ca0439816ac56d5.

--- SymbolTable.hpp
+++ SymbolTable.hpp
@@ -7,17 +7,27 @@
 
 class SymbolTable {
 private:
+  struct VarData{
+    double value;
+    int line_num;
+  };
   // CODE TO STORE SCOPES AND VARIABLES HERE.
   
   // HINT: YOU CAN CONVERT EACH VARIABLE NAME TO A UNIQUE ID TO CLEANLY DEAL
   //       WITH SHADOWING AND LOOKING UP VARIABLES LATER.
+  std::vector<std::unordered_map<std::string, double>> scope;
 
 public:
   // CONSTRUCTOR, ETC HERE
-
+  SymbolTable(){} 
   // FUNCTIONS TO MANAGE SCOPES (NEED BODIES FOR THESE IF YOU WANT TO USE THEM)
-  void PushScope() { ; }
-  void PopScope() { ; }
+  void PushScope(std::unordered_map<std::string, double> mp) { scope.push_back(mp); }
+  std::unordered_map<std::string, double> PopScope()
+  { 
+    auto temp = scope.back(); 
+    scope.pop_back(); 
+    return temp;
+  }
 
   // FUNCTIONS TO MANAGE VARIABLES - LOTS MORE NEEDED
   // (NEED REAL FUNCTION BODIES FOR THESE IF YOU WANT TO USE THEM)
