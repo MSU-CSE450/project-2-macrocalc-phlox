2cd4707ffa932c02191486020ca0439816ac56d5
komandu5 <komandu5@msu.edu>
2024-10-05 11:21:51 -0400

    Co-authored-by: Alex Banker <AlexBanker@users.noreply.github.com> Co-authored-by: Rohit Chavan <chavanro@msu.edu> Co-authored-by: Joe Tanquary <joetanquary@users.noreply.github.com>

--- Project2.cpp
+++ Project2.cpp
@@ -9,11 +9,47 @@
 // You may delete this and divide it up however you like.
 #include "lexer.hpp"
 #include "SymbolTable.hpp"
+#include "ASTNode.hpp"
 
-void Error(size_t line_num, std::string message) {                // https://github.com/MSU-CSE450/WordLang/blob/main/WordLang.cpp
-  std::cerr << "ERROR (line " << line_num << "): " << message << std::endl;
-  exit(1);
-}
+// class Parser {
+//   private:
+//     size_t token_id{0};
+//     std::vector<emplex::Token> tokens;
+
+//   public:
+//     Parser(std::vector<emplex::Token> tokens) {
+//       tokens = tokens;
+
+//     }
+
+//     emplex::Token CurrentToken() {return tokens[token_id]; }
+
+//     ASTNode ParseStatement() // Taken from WordLang
+//     { 
+//         // Lexer
+//         // switch (CurrentToken()) {
+//         //   using namespace emplex;
+//         //   case Lexer::ID_PRINT: return ParsePrint();
+//         //   case Lexer::ID_TYPE: return ParseDeclare();
+//         //   case Lexer::ID_FOREACH: return ParseForeach();
+//         //   case ';': return ASTNode{0};
+//         //   default:
+//         //     return ParseExpression();
+//         //}
+//       }
+
+
+
+  
+// };
+
+// // void Error(size_t line_num, std::string message) {                // https://github.com/MSU-CSE450/WordLang/blob/main/WordLang.cpp
+// //   std::cerr << "ERROR (line " << line_num << "): " << message << std::endl;
+// //   exit(1);
+// // }
+
+
+
 
 
 bool endsWith(const std::string& fullString, const std::string& ending) //https://www.geeksforgeeks.org/check-if-string-ends-substring-in-cpp/
@@ -28,13 +64,13 @@ bool endsWith(const std::string& fullString, const std::string& ending) //https:
     return fullString.compare(fullString.size() - ending.size(), ending.size(), ending) == 0;
 }
 
-template <typename... Ts>
-void Error(size_t line_num, Ts... message) {
-  std::cerr << "ERROR (line " << line_num << "): ";
-  (std::cerr << ... << message);
-  std::cerr << std::endl;
-  exit(1);
-}
+// template <typename... Ts>
+// void Error(size_t line_num, Ts... message) {
+//   std::cerr << "ERROR (line " << line_num << "): ";
+//   (std::cerr << ... << message);
+//   std::cerr << std::endl;
+//   exit(1);
+// }
 int main(int argc, char * argv[])
 {
   if (argc != 2) {
@@ -52,14 +88,21 @@ int main(int argc, char * argv[])
   std::ifstream input(filename);
   emplex::Lexer lexer;
   std::vector<emplex::Token> tokens = lexer.Tokenize(input);
+  //auto symbol_table = SymbolTable();
   std::string output_string;
-  auto symbol_table = SymbolTable();
+  bool print_active = false;
+  bool condition_active = false;
+  std::string current_variable = "";
+  bool value_change = false;
+  std::map<std::string, std::string> mp;
   for (emplex::Token token: tokens)
   {
-    
-  }
-  /*
-  if(token.id != -1){
+    if(token.id == 245) //If start condition active
+    {
+      //auto A = ASTNode(1);
+      //std::cout << A.NodeType() << std::endl;
+      if(token.id != -1)
+      {
       if(token.id == 252) //ID Print detected
       {
         print_active = true; //says print is active
@@ -91,7 +134,7 @@ int main(int argc, char * argv[])
       }
       else if(condition_active and token.id == 241) //if an equation in print statement
       {
-        AST(token.lexeme, mp);
+        //AST(token.lexeme, mp);
       }
       else if(token.id == 246) //semicolon (end of line) detected 
       {
@@ -131,8 +174,13 @@ int main(int argc, char * argv[])
         }
       }
     }
+     
+  }
+  /*
+  
   */
   // TO DO:  
   // PARSE input file to create Abstract Syntax Tree (AST).
   // EXECUTE the AST to run your program.
+}
 }
