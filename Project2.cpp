05f023803c170302a1351e9088125cc48d0f6bcc
komandu5 <komandu5@msu.edu>
2024-10-14 08:49:41 -0400

    minor changes

--- Project2.cpp
+++ Project2.cpp
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -7,213 +8,512 @@
 
 // Below are some suggestions on how you might want to divide up your project.
 // You may delete this and divide it up however you like.
-#include "lexer.hpp"
-#include "SymbolTable.hpp"
 #include "ASTNode.hpp"
+#include "SymbolTable.hpp"
+#include "lexer.hpp"
 
-// class Parser {
-//   private:
-//     size_t token_id{0};
-//     std::vector<emplex::Token> tokens;
-
-//   public:
-//     Parser(std::vector<emplex::Token> tokens) {
-//       tokens = tokens;
-
-//     }
-
-//     emplex::Token CurrentToken() {return tokens[token_id]; }
-
-//     ASTNode ParseStatement() // Taken from WordLang
-//     { 
-//         // Lexer
-//         // switch (CurrentToken()) {
-//         //   using namespace emplex;
-//         //   case Lexer::ID_PRINT: return ParsePrint();
-//         //   case Lexer::ID_TYPE: return ParseDeclare();
-//         //   case Lexer::ID_FOREACH: return ParseForeach();
-//         //   case ';': return ASTNode{0};
-//         //   default:
-//         //     return ParseExpression();
-//         //}
-//       }
-
-
-
-  
-// };
-
-// // void Error(size_t line_num, std::string message) {                // https://github.com/MSU-CSE450/WordLang/blob/main/WordLang.cpp
-// //   std::cerr << "ERROR (line " << line_num << "): " << message << std::endl;
-// //   exit(1);
-// // }
-
-
-
-
-
-bool endsWith(const std::string& fullString, const std::string& ending) //https://www.geeksforgeeks.org/check-if-string-ends-substring-in-cpp/
-{
-    // Check if the ending string is longer than the full
-    // string
-    if (ending.size() > fullString.size())
-        return false;
-
-    // Compare the ending of the full string with the target
-    // ending
-    return fullString.compare(fullString.size() - ending.size(), ending.size(), ending) == 0;
+template <typename... Ts>
+void Error(size_t line_num, Ts... message) {
+  std::cerr << "ERROR (line " << line_num << "): ";
+  (std::cerr << ... << message);
+  std::cerr << std::endl;
+  exit(1);
 }
 
-// template <typename... Ts>
-// void Error(size_t line_num, Ts... message) {
-//   std::cerr << "ERROR (line " << line_num << "): ";
-//   (std::cerr << ... << message);
-//   std::cerr << std::endl;
-//   exit(1);
-// }
-int main(int argc, char * argv[])
-{
+class MacroCalc {
+ private:
+  size_t token_id = 0;
+  std::vector<emplex::Token> tokens{};
+  //ASTNode root{ASTNode::STATEMENT_BLOCK};
+
+  SymbolTable symbols{};
+
+  // === HELPER FUNCTIONS ===
+
+  std::string TokenName(int id) const {
+    if (id > 0 && id < 128) {
+      return std::string("'") + static_cast<char>(id) + "'";
+    }
+    return emplex::Lexer::TokenName(id);
+  }
+
+  emplex::Token CurToken() const { return tokens[token_id]; }
+
+  emplex::Token UseToken() { return tokens[token_id++]; }
+
+  emplex::Token UseToken(int required_id, std::string err_message = "") {
+    if (CurToken() != required_id) {
+      if (err_message.size())
+        Error(CurToken(), err_message);
+      else {
+        Error(CurToken(), "Expected token type ", TokenName(required_id),
+              ", but found ", TokenName(CurToken()));
+      }
+    }
+    return UseToken();
+  }
+
+  bool UseTokenIf(int test_id) {
+    if (CurToken() == test_id) {
+      token_id++;
+      return true;
+    }
+    return false;
+  }
+
+  // ASTNode MakeVarNode(const emplex::Token& token) {
+  //   size_t var_id = symbols.GetVarID(token.lexeme);
+  //   assert(var_id < symbols.GetNumVars());
+  //   ASTNode out(ASTNode::VAR);
+  //   out.SetVal(var_id);
+  //   return out;
+  // }
+
+ public:
+  MacroCalc(std::string filename) {  // Looked at WordLang.cpp for this
+    std::ifstream file(filename);
+    emplex::Lexer lexer;
+    tokens = lexer.Tokenize(file);
+
+    Parse();
+  }
+  void Parse() {
+    for(auto token : tokens)
+    {
+      ParseStatement();
+    }
+  }
+
+  void ParseStatement() {
+    switch (CurToken()) {
+      using namespace emplex;
+      case Lexer::ID_Print:
+      {
+        ParsePrint();
+        break;
+      }
+      case Lexer::ID_Var:
+      {
+        ParseDeclare();
+        break;
+      }
+      case Lexer::ID_Statement: {
+        ParseIf();
+        break;
+      }
+      
+      case Lexer::ID_VariableName: {
+        ParseNewVal();
+        break;
+      }
+      // case Lexer::ID_WHILE: return ParseWhile();
+      // case '{':
+      //   //return ParseStatementBlock();
+      // case ';':
+      //   return ASTNode{};
+      default:
+      {
+        ParseExpression();
+        break;
+      }
+    }
+  }
+  bool while_enabled = false;
+  void ParseIf()
+  {
+    auto type = UseToken(emplex::Lexer::ID_Statement);
+    UseToken(emplex::Lexer::ID_StartCondition);
+    if(type.lexeme == "if")
+    {
+      auto object = ParseExpression();
+
+      auto comp = UseToken(emplex::Lexer::ID_Equivalent);
+      if(comp.lexeme == "==")
+      {
+        auto object2 = UseToken();
+
+        if(object == std::stod(object2.lexeme))
+        {
+          UseToken(emplex::Lexer::ID_EndCondition);
+          ParseStatement();
+        }
+        else
+        {
+          while(CurToken().lexeme != ";")
+          {
+            UseToken();
+          }
+          UseToken(emplex::Lexer::ID_EOL);
+        }
+      }
+    }
+    else if(type.lexeme == "while")
+      {
+        //std::cout << ParseEquiv();
+        UseToken(emplex::Lexer::ID_EndCondition);
+        std::cout << CurToken().lexeme;
+        while(ParseEquiv() == 1)
+        {
+          //UseToken();
+          //ParseStatement();
+          
+        }
+
+        
+        // else
+        // {
+        //   while(CurToken().lexeme != ";")
+        //   {
+        //     UseToken();
+        //   }
+        //   UseToken(emplex::Lexer::ID_EOL);
+        // }
+      }
+    
+  }
+  bool isScope = false;
+  void ParsePrint() {
+    UseToken(emplex::Lexer::ID_Print);
+    UseToken(emplex::Lexer::ID_StartCondition);
+    
+    if (CurToken().id == emplex::Lexer::ID_LitString) {
+        // Handle string output with variable replacement
+        std::string output = "";
+        std::string currvar = "";
+        for(auto ch : CurToken().lexeme){
+          if(ch != '"'){
+            if(ch == '{')
+            {
+              isScope = true;
+            }
+            else if(ch == '}')
+            {
+              int i = std::to_string(symbols.GetValue(currvar)).size();
+              std::string intermediate = std::to_string(symbols.GetValue(currvar));
+              std::string last_dig = "";
+              while(intermediate.back() == '0')
+              {
+                if(intermediate.back() == '0')
+                {
+                  intermediate.pop_back();
+                }
+              }
+              last_dig = intermediate.back();
+              if(last_dig == ".")
+                {
+                  intermediate.pop_back();
+                }
+              output += intermediate;
+              
+              currvar = "";
+              isScope = false;
+            }
+            else if(isScope == false)
+            {
+              output += ch;
+            }
+            else
+            {
+              currvar += ch;
+            }
+          }
+        }
+        UseToken(emplex::Lexer::ID_LitString);
+        std::cout << output << std::endl;
+     } 
+    else {
+        // Handle expression output
+        double value = ParseAnd();
+        std::cout << value << std::endl;
+    }
+    UseToken(emplex::Lexer::ID_EndCondition);
+    UseToken(emplex::Lexer::ID_EOL);  
+
+  }
+
+  void ParseDeclare() {
+        UseToken(emplex::Lexer::ID_Var);  
+        std::string varName = CurToken().lexeme;
+        UseToken(emplex::Lexer::ID_VariableName);  
+        if(!symbols.HasVar(varName))
+        {
+          symbols.AddVar(varName);
+
+          if(CurToken().id == emplex::Lexer::ID_Equal) {
+              UseToken(emplex::Lexer::ID_Equal);  
+              double value = ParseExpression(); 
+              symbols.SetValue(varName, value); 
+              
+          }
+          else
+          {
+            Error(CurToken(), "left side must be variable");
+          }
+          UseToken(emplex::Lexer::ID_EOL);
+        }
+        else{
+          Error(CurToken(), "Redeclaring Variable");
+        }
+         
+    }
+
+  double ParseExpression() {
+          return ParseAddition();
+      }
+  double ParseAnd(){
+    double left = ParseEquiv();
+    while(CurToken().lexeme == "&&" or CurToken().lexeme == "||"){
+      auto op = CurToken().lexeme;
+      UseToken();
+      double right = ParseEquiv();
+      if(CurToken().lexeme == "&&")
+      {
+        if(left == 0)
+        {
+          return 0;
+        }
+        else if(left != right)
+        {
+          return 0;
+        }
+        else if(right == 0)
+        {
+          return 0;
+        }
+        else if(left == 1 and right == 1)
+        {
+          return 1;
+        }
+      }
+      else if(CurToken().lexeme == "||")
+      {
+        if(left == 1)
+        {
+          return 1;
+        }
+        else if(right == 1)
+        {
+          return 1;
+        }
+        else if(left == 1 and right == 1)
+        {
+          return 1;
+        }
+        else if(left == 0 and right == 1)
+        {
+          return 1;
+        }
+        else
+        {
+          return 0;
+        }
+      }
+    }
+    return left;
+  }
+  double ParseEquiv() {
+    if(CurToken().lexeme == "!")
+    {
+      double right = ParseAnd();
+      if(right != 0)
+      {
+        return 0;
+      }
+      else
+      {
+        return 1;
+      }
+    }
+    double left = ParseAddition();
+    while (CurToken().lexeme == "==" or CurToken().lexeme == "!=" or CurToken().lexeme == ">" or CurToken().lexeme == "<" or CurToken().lexeme == ">=" or CurToken().lexeme == "<=") {
+          auto op = CurToken().lexeme;
+          UseToken(CurToken().id);
+          double right = ParseAddition();
+
+          if (op == "==") {
+              if(left == right)
+              {
+                return 1;
+              }
+              else
+              {
+                return 0;
+              }
+          } 
+          else if(op == "!="){
+              if(left != right)
+              {
+                return 1;
+              }
+              else
+              {
+                return 0;
+              }
+          }
+          else if(op == ">"){
+              if(left > right)
+              {
+                return 1;
+              }
+              else
+              {
+                return 0;
+              }
+          }
+          else if(op == ">="){
+              if(left >= right)
+              {
+                return 1;
+              }
+              else
+              {
+                return 0;
+              }
+          }
+          else if(op == "<="){
+              if(left <= right)
+              {
+                return 1;
+              }
+              else
+              {
+                return 0;
+              }
+          }
+          else if(op == "<"){
+              if(left < right)
+              {
+                return 1;
+              }
+              else
+              {
+                return 0;
+              }
+          }
+          
+      }
+      return left;
+  }
+  void ParseNewVal()
+  {
+    std::string varName = CurToken().lexeme;
+    UseToken(emplex::Lexer::ID_VariableName);  
+    if(symbols.HasVar(varName))
+    {
+      if(CurToken().id == emplex::Lexer::ID_Equal) {
+          UseToken(emplex::Lexer::ID_Equal);  
+          double value = ParseExpression(); 
+          symbols.SetValue(varName, value); 
+          
+      }
+      else
+      {
+        Error(CurToken(), "left side must be variable");
+      }
+      UseToken(emplex::Lexer::ID_EOL);
+    }
+    else{
+      Error(CurToken(), "Redeclaring Variable");
+    }
+  }
+    // Parse additive expressions (e.g., addition and subtraction)
+  double ParseAddition() {
+      double left = ParseMult();
+
+      while (CurToken().lexeme == "+" or CurToken().lexeme == "-") {
+          auto op = CurToken().lexeme;
+          UseToken(CurToken().id);  // Consume '+' or '-'
+          double right = ParseMult();
+
+          if (op == "+") {
+              left += right;
+          } else {
+              left -= right;
+          }
+      }
+
+      return left;
+  }
+
+  double ParseMult() {
+      double left = ParsePrim();
+
+      while (CurToken().lexeme == "*" or CurToken().lexeme == "/" or CurToken().lexeme == "**" or CurToken().lexeme == "%") {
+          auto op = CurToken().lexeme;
+          UseToken(CurToken().id);  // Consume '*', '/', or '**'
+          double right = ParsePrim();
+
+          if (op == "*") {
+              left *= right;
+          } 
+          else if (op == "/") {
+              if (right == 0) {
+                  Error(CurToken(),"Divide by zero");
+              }
+              left /= right;
+          } 
+          else if (op == "**") {
+              left = pow(left, right);
+          }
+          else if (op == "%") {
+              if (right == 0) {
+                  Error(CurToken(),"Divide by zero");
+              }
+              return fmod(left, right);
+
+          } 
+      }
+
+      return left;
+  }
+
+  // Parse primary expressions (e.g., numbers, variables, or parenthesized expressions)
+  double ParsePrim() {
+      if (CurToken().id == emplex::Lexer::ID_Value) {
+        double value = std::stod(CurToken().lexeme);  // Convert token to double
+        UseToken(emplex::Lexer::ID_Value);  // Consume the numeric value token
+        return value;
+      } 
+    else if (CurToken().id == emplex::Lexer::ID_VariableName) {
+        std::string varName = CurToken().lexeme;
+        UseToken(emplex::Lexer::ID_VariableName);
+
+        if (!symbols.HasVar(varName)) {
+            Error(token_id, "Undefined variable: ", varName);
+        }
+        return symbols.GetValue(varName);  // Return the value of the variable
+    } 
+    else if (CurToken().id == emplex::Lexer::ID_StartCondition) {
+        UseToken(emplex::Lexer::ID_StartCondition);
+        double expr = ParseExpression();  // Parse expression inside parentheses
+        UseToken(emplex::Lexer::ID_EndCondition);  // Expect closing parenthesis
+        return expr;
+    }
+    else if(CurToken().id != emplex::Lexer::ID__EOF_){
+        Error(token_id, "Unexpected token in primary expression: ", TokenName(CurToken().id));
+    }
+  }
+};
+
+int main(int argc, char* argv[]) {
   if (argc != 2) {
     std::cout << "Format: " << argv[0] << " [filename]" << std::endl;
     exit(1);
   }
 
   std::string filename = argv[1];
-  
-  std::ifstream in_file(filename);              // Load the input file
+
+  std::ifstream in_file(filename);  // Load the input file
   if (in_file.fail()) {
-    std::cout << "ERROR: Unable to open file '" << filename << "'." << std::endl;
+    std::cout << "ERROR: Unable to open file '" << filename << "'."
+              << std::endl;
     exit(1);
   }
-  std::ifstream input(filename);
-  emplex::Lexer lexer;
-  std::vector<emplex::Token> tokens = lexer.Tokenize(input);
-  //auto symbol_table = SymbolTable();
-  std::string output_string;
-  bool print_active = false;
-  bool condition_active = false;
-  std::string current_variable = "";
-  bool value_change = false;
-  std::unordered_map<std::string, std::string> mp;
-  bool equal = false;
-  bool statement = false;
-  for (emplex::Token token: tokens)
-  {
-    if(token.id != -1){
-      if(token.id == 252 && !print_active) //ID Print detected
-      {
-        print_active = true; //says print is active
-      }
-      else if(token.id == 255 && !statement)
-      {
-        statement = true;
-      }
-      else if(statement == true)
-      {
-        if(token.id == 255)
-        {
-          std::cerr << "cannot put conditional within condtitional" << std::endl;
-          exit(1);
-        }
-      }
-      else if(print_active && token.id == 245) //detects open parentheses for print statement
-      {
-        condition_active = true;
-      }
-      else if(token.id == 243) //detects close parentheses
-      {
-        condition_active = false;
-      }
-      else if(condition_active && token.id == 244 && print_active) //if printing a literal string
-      {
-        for(auto ch : token.lexeme)
-        {
-          if(ch != '"'){
-            output_string += ch;
-          }
-        }
-      }
-      else if(condition_active && token.id == 247 && print_active) //if printing number
-      {
-        output_string += token.lexeme;
-      }
-      else if(condition_active && token.id == 249 && print_active) //if printing a variable
-      {
-        output_string += mp[token.lexeme];
-        current_variable = token.lexeme;
-      }
-      else if(condition_active && token.id == 241) //if an equation in print statement
-      {
-        //AST(token.lexeme, mp);
-        std::cout << current_variable;
-      }
-      else if(token.id == 246 && condition_active == false) //semicolon (end of line) detected 
-      {
-        current_variable = "";
-        value_change = false;
-        if(print_active) //if print statement
-        {
-          print_active = false;
-          std::cout << output_string << std::endl;
-          condition_active = false;
-          output_string = "";
-        }
-      }
-      else if(print_active && token.id == 252)
-      {
-        std::cerr << std::endl;
-        exit(1);
-      }
-      else if(token.id == 251) //var keyword detected
-      {
-        value_change = true; //indicates value is being changed
-      }
-      else if(value_change == true) //if a value change is ongoing
-      {
-        if(token.id == 249 && current_variable == "") // if a variable name is detected
-        {
-          current_variable = token.lexeme; //current variable is set
-        }
-        else if(token.id == 249 && current_variable != "" && equal)
-        {
-          mp[current_variable] = mp[token.lexeme];
-        }
-        else if (token.id == 248)
-        {
-          equal = true;
-        }
-        else if(token.id == 249 && !equal)
-        {
-          std::cerr << "AAAAAA" << std::endl;
-          exit(1);
-        }
-        else if(token.id == 247 && current_variable == "")
-        {
-          std::cerr << "You cannot make a variable name a number" << std::endl;
-          exit(1);
-        }
-        else if(token.id == 247 && current_variable != "" && equal) //if a value is detected
-        {
-          std::string extra = ".0";
-          if(endsWith(token.lexeme, extra)) //if value ends with ".0"
-          {
-            token.lexeme.pop_back(); // removing trailing 0
-            token.lexeme.pop_back(); // remove decimal
-          }
-          mp[current_variable] = token.lexeme; //sets map value to token.lexeme
-        }
-      }
-    }
-    // if(token.id == 245) //If start condition active
-    // {
-    //   //auto A = ASTNode(1);
-    //   //std::cout << A.NodeType() << std::endl;
-    // }
-  /*
-  
-  */
-  // TO DO:  
+
+  // TO DO:
   // PARSE input file to create Abstract Syntax Tree (AST).
   // EXECUTE the AST to run your program.
+
+  MacroCalc calc(filename);
 }
-}
+
--- SymbolTable.hpp
+++ SymbolTable.hpp
@@ -23,7 +23,6 @@ class SymbolTable {
 private:
   struct VarData{
     std::string name;
-    double var_val;
     size_t line_num;
   };
   // CODE TO STORE SCOPES AND VARIABLES HERE.
@@ -31,50 +30,52 @@ private:
   // HINT: YOU CAN CONVERT EACH VARIABLE NAME TO A UNIQUE ID TO CLEANLY DEAL
   //       WITH SHADOWING AND LOOKING UP VARIABLES LATER.
   std::vector<VarData> var_info;
-  using scope_t = std::unordered_map<std::string, size_t>;
-  std::vector<std::unordered_map<std::string, size_t>> scope{1};//size_t represents ID, like 247
+  std::vector<std::unordered_map<std::string, double>> scope{1};
 
 public:
   // CONSTRUCTOR, ETC HERE
   SymbolTable(){} 
   static constexpr size_t NO_ID = static_cast<size_t>(-1);
-
   // FUNCTIONS TO MANAGE SCOPES
-  void IncScope() { scope.emplace_back(); }
-  void DecScope() { scope.pop_back(); }
-  void PushScope(std::unordered_map<std::string, size_t> mp) { scope.push_back(mp); }
-  std::unordered_map<std::string, size_t> PopScope(){ 
+  void PushScope(std::unordered_map<std::string, double> mp) { scope.push_back(mp); }
+  std::unordered_map<std::string, double> PopScope()
+  { 
     auto temp = scope.back(); 
     scope.pop_back(); 
     return temp;
   }
-  
   // FUNCTIONS TO MANAGE VARIABLES
   size_t GetNumVars() const { return var_info.size(); }
   size_t GetVarID(std::string name) const {
-    for (auto it = scope.rbegin(); it != scope.rend(); ++it){
+    for (auto it = scope.rbegin();
+         it != scope.rend();
+         ++it)
+    {
       if (it->count(name)) return it->find(name)->second;
     }
+
     return NO_ID;
   }
   bool HasVar(std::string name) const { return (GetVarID(name) != NO_ID); }
-  size_t AddVar(std::string name, size_t line_num) { 
+  size_t AddVar(std::string name) { 
     auto &curr_scope = scope.back();
     if (curr_scope.count(name)) {
-      std::cerr << "ERROR (line " << line_num << "): Redeclaring variable '" << name << "'." << std::endl;
+      std::cerr << "ERROR"<< ": Redeclaring variable '" << name << "'." << std::endl;
     }
     size_t var_id = var_info.size();
-    var_info.emplace_back(name, line_num);
+    var_info.emplace_back(VarData {name});
     curr_scope[name] = var_id;
     return var_id;
   }
   double GetValue(std::string name) const {
     assert(HasVar(name));
-    return 0.0;
+    auto mp = scope.back();
+    return mp[name];
   }
   void SetValue(std::string name, double value) { 
-    std::unordered_map<std::string, size_t> curr_map = PopScope();
+    std::unordered_map<std::string, double> curr_map = PopScope();
     curr_map[name] = value;
     PushScope(curr_map);
   }
 };
+
